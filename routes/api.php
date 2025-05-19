13312247b36b228b2154fbb18b17a6afd90c0e6c
Tayo Adepetu <copyafrca93@gmail.com>
2025-05-19 22:27:53 +0100

    auth guard

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Authentication\LoginController;
 
-Route::post('register', [LoginController::class, 'register']);
+Route::post('/register', [LoginController::class, 'register']);
 Route::post('login', [LoginController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
