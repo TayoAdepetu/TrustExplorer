19bdf62f74ba7d493d2b82dd90debb753853f4ef
Tayo Adepetu <copyafrca93@gmail.com>
2025-05-20 17:44:17 +0100

    auth guard

--- resources/views/email/password_reset_link.blade.php
+++ resources/views/email/password_reset_link.blade.php
@@ -21,7 +21,7 @@
                     <tbody>
                       <tr>
                         <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #008952; border-radius: 5px; text-align: center;">
-                          <a href="{{$body['email_verification_link']}}" target="_blank" style="display: inline-block; color: #ffffff; background-color: #008952; border: solid 1px #008952; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #008952;">
+                          <a href="{{$body['password_reset_link']}}" target="_blank" style="display: inline-block; color: #ffffff; background-color: #008952; border: solid 1px #008952; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #008952;">
                             Reset Password
                           </a>
                         </td>
