9b56fc92079a7c6d4c236d2e5654bd3c223cce0c
Tayo Adepetu <copyafrca93@gmail.com>
2025-05-20 09:06:15 +0100

    auth guard

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,6 @@ Route::post('/login', 'Authentication\LoginController@loginUser');
 Route::get('/get-user-details/{user_ref}', 'Authentication\LoginController@getUserDetail');
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/logout', 'Authentication\LoginController@logout');
+    Route::post('/logout', 'Authentication\LoginController@logout');
     Route::get('/refresh', 'Authentication\LoginController@refresh');
 });
