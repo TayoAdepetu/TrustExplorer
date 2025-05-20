b687c12cfd1af7856307ba39452c0b7661cd2c66
Tayo Adepetu <copyafrca93@gmail.com>
2025-05-20 17:42:25 +0100

    auth guard

--- resources/views/email/password_reset_link.blade.php
+++ resources/views/email/password_reset_link.blade.php
@@ -5,7 +5,7 @@
     <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
       <tr>
         <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
-          <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Hi {{$firstname}},</p>
+          <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Hi {{$body['first_name']}},</p>
           <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">
             <b>A password reset for the account associated with this email has been requested.
               <br />Please reset your password by clicking the button below.</b>
