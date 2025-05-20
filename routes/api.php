3cc5eee01b0024bf17abd75797426bcd1d0a61d4
Tayo Adepetu <copyafrca93@gmail.com>
2025-05-20 18:10:11 +0100

    auth guard

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::post('/register', 'Authentication\RegisterController@registerUser');
 Route::post('/login', 'Authentication\LoginController@loginUser');
 Route::get('/get-user-details/{user_ref}', 'Authentication\LoginController@getUserDetail');
 Route::post('/send-password-reset-token', 'Authentication\RegisterController@sendPasswordResetToken');
-Route::post('/reset-password', 'Authentication\RegisterController@sendPasswordResetToken');
+Route::post('/reset-password', 'Authentication\RegisterController@setNewAccountPassword');
 
 Route::middleware(['auth:api'])->group(function () {
     Route::post('/logout', 'Authentication\LoginController@logout');
